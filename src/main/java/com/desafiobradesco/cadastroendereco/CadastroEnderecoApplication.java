36365f78b7a50a9000619775d9ae76bdadc1e6ad
João Augusto <joaomoreira@controlese.com.br>
2023-04-13 17:52:37 -0300

    Initial Commit

---
+++ src/main/java/com/desafiobradesco/cadastroendereco/CadastroEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.desafiobradesco.cadastroendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEnderecoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroEnderecoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desafiobradesco/cadastroendereco/CadastroEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiobradesco.cadastroendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEnderecoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
